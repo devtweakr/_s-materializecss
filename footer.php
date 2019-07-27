0f5b35b0ea4390fe4cc210b7f25903ce7ed65b5d
Nathan Parikh <ndzynes@gmail.com>
2019-07-27 15:57:49 -0500

    Footer Updates. Added pagination function to utilize MaterilizeCSS.

--- footer.php
+++ footer.php
@@ -24,7 +24,7 @@
 			<span class="sep"> | </span>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', '_s-materializecss' ), '_s-materializecss', '<a href="http://underscores.me/">Underscores.me</a>' );
+				printf( esc_html__( 'Theme: %1$s by %2$s.', '_s-materializecss' ), '_s-materializecss', '<a href="https://ndzynes.com/">nDzynes</a>' );
 				?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
--- inc/template-functions.php
+++ inc/template-functions.php
@@ -35,3 +35,36 @@ function _s_materializecss_pingback_header() {
 	}
 }
 add_action( 'wp_head', '_s_materializecss_pingback_header' );
+
+/**
+ * Custom pagination
+ */
+function _s_materializecss_page_navi() {
+	global $wp_query;
+    $big = 999999999; // need an unlikely integer
+    $pagination_items =  paginate_links( array(
+        'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+        'format' => '?paged=%#%',
+        'current' => max( 1, get_query_var('paged') ),
+        'total' => $wp_query->max_num_pages,
+        'type'               => 'array',
+        'prev_text'          => __('<i class="material-icons">chevron_left</i>'),
+        'next_text'          => __('<i class="material-icons">chevron_right</i>'),
+        'before_page_number' => '',
+        'after_page_number'  => ''
+    ) );
+    echo '<ul class="pagination center-align">';
+    if($pagination_items != null) {
+        foreach ($pagination_items as $item) {
+            if (strpos($item, '<span') === false) {
+                echo '<li class="waves-effect">' . $item . '</li>';
+            } else {
+                $item = str_replace('<span', '<a href="#"', $item);
+                $item = str_replace('</span>', '</a>', $item);
+                echo '<li class="active">' . $item . '</li>';
+            }
+        }
+    }
+    echo '</ul>';
+}
+
